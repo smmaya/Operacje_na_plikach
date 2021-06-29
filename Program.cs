d155308af297f7110d487ca734f95e0e1e3bd86f
Sławomir Majchrzak <52676366+smmaya@users.noreply.github.com>
2021-06-29 12:50:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+//Programik podnosi do kwadratu liczby od 1 do 100 i zapisuje wyniki do pliku tekstowego.
+
+namespace Kwadraty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sciezka = "/Users/Maya/programowanie/wynik.txt";
+            using (StreamWriter sw = new StreamWriter(sciezka))
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    sw.WriteLine("kwadrat " + i + " to: " + (i * i));
+                }
+            }
+        }
+    }
+}
